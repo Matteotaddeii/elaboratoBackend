faaa01fc6bddd0a56e2d8feaeae47a7912563ff3
matteotaddeii <matteo.taddei1@edu.unifi.it>
2026-05-29 18:52:16 +0200

    Crwati modelli per Store e fatte migrazioni

--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, help_text="Url amichevole, es: 'informatica'")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    # Prodotto appartiene a una categoria
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    # Ordine appartiene a un utente
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"Ordine {self.id} - {self.user.username}"
+
+class OrderItem(models.Model):
+    # Dettagli dell'ordine
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
