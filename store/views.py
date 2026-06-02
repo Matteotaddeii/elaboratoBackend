a5d386369af8c7a7f6ad5517c4f74eceedb578ed
matteotaddeii <matteo.taddei1@edu.unifi.it>
2026-06-02 11:34:24 +0200

    Implementata vista per mostrare i dettagli dei prodotti

--- store/views.py
+++ store/views.py
@@ -1,10 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 class ProductListView(ListView):
     model = Product
     template_name = 'catalogo.html'
     
-    # Filtriamo per mostrare solo i prodotti attivi
+    # Mostra solo i prodotti attivi
     def get_queryset(self):
         return Product.objects.filter(is_active=True)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'dettagliProdotto.html'
+    context_object_name = 'product' 
