3a4ce5a6002267f71586239b1294ca2481b7ba51
matteotaddeii <matteo.taddei1@edu.unifi.it>
2026-06-03 19:57:38 +0200

    Implementata possibilita per utente di modificare/eliminare le recensioni fatte. Store manager puo eliminare le recensioni di tutti.

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('aggiungi/<int:product_id>/', views.add_review, name='add_review'),
+    path('cancella/<int:review_id>/', views.delete_review, name='delete_review'),
+    path('modifica/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from store.models import Product
+from .models import Review
 from .forms import ReviewForm
 
 @login_required
@@ -20,3 +21,43 @@ def add_review(request, product_id):
             messages.error(request, "Errore durante l'invio della recensione. Controlla i dati.")
             
     return redirect('product_detail', pk=product.id)
+
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    product_id = review.product.id  
+    
+    if request.user == review.user or getattr(request.user, 'role', '') == 'store_manager' or request.user.is_superuser:
+        
+        review.delete()
+        messages.success(request, "Recensione eliminata definitivamente dal database.")
+    else:
+        messages.error(request, "Azione non consentita. Non hai i permessi per eliminare questa recensione.")
+        
+    return redirect('product_detail', pk=product_id)
+
+@login_required
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    product = review.product
+    
+    if request.user != review.user:
+        messages.error(request, "Azione non consentita. Puoi modificare solo le tue recensioni.")
+        return redirect('product_detail', pk=product.id)
+        
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Recensione aggiornata con successo!")
+            return redirect('product_detail', pk=product.id)
+        else:
+            messages.error(request, "Errore nella compilazione. Controlla i dati inseriti.")
+    else:
+        form = ReviewForm(instance=review)
+        
+    return render(request, 'modificaRecensione.html', {
+        'form': form,
+        'review': review,
+        'product': product
+    })    
